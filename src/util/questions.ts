9552e46aaf6f566744727bd9d8448ad7532c95d6
ivanwritescode <ivanmejico.dev@gmail.com>
2023-06-20 13:35:49 +0800

    Added more interview questions

--- src/util/questions.ts
+++ src/util/questions.ts
@@ -90,4 +90,148 @@ export const interviewquestions = [
         "correctAnswer": "To handle side effects and perform cleanup in functional components",
         "explanation": "The useEffect hook in React is used to handle side effects in functional components. Side effects may include data fetching, subscriptions, or manually changing the DOM. It allows you to perform certain actions after React has rendered and updated the DOM. Additionally, the useEffect hook can also be used to clean up resources and perform actions before the component is unmounted or updated."
     },
+    {
+        "id": 7,
+        "type": "multipleChoice",
+        "question": "What is the purpose of the useCallback hook in React?",
+        "options": [
+            "To memoize expensive calculations",
+            "To create reusable custom hooks",
+            "To handle form validation in React applications",
+            "To handle side effects and perform cleanup in functional components"
+        ],
+        "correctAnswer": "To memoize expensive calculations",
+        "explanation": "The useCallback hook in React is used to memoize expensive calculations. It is particularly useful when passing callbacks to child components that rely on the parent component's state or props. By wrapping the callback function with useCallback, React can memoize the function and only recreate it if the dependencies specified in the dependency array change. This optimization can help improve performance by avoiding unnecessary re-renders of child components."
+    },
+    {
+        "id": 8,
+        "type": "multipleChoice",
+        "question": "What is the purpose of the useMemo hook in React?",
+        "options": [
+            "To memoize expensive calculations",
+            "To create reusable custom hooks",
+            "To handle form validation in React applications",
+            "To handle side effects and perform cleanup in functional components"
+        ],
+        "correctAnswer": "To memoize expensive calculations",
+        "explanation": "The useMemo hook in React is used to memoize expensive calculations. It allows you to optimize the performance of your application by caching the result of a function and returning the cached value when the inputs (dependencies) haven't changed. This can be especially helpful when dealing with computationally intensive operations or when the calculation relies on complex data structures. By memoizing the result, React can avoid unnecessary re-computation and improve the overall efficiency of the application."
+    },
+    {
+        "id": 9,
+        "type": "multipleChoice",
+        "question": "What is JSX in React?",
+        "options": [
+            "A JavaScript syntax extension",
+            "A CSS-in-JS library",
+            "A component lifecycle method",
+            "A state management library"
+        ],
+        "correctAnswer": "A JavaScript syntax extension",
+        "explanation": "JSX (JavaScript XML) is a syntax extension used in React to describe the structure of UI components. It allows developers to write HTML-like code in JavaScript, making it easier to define the structure and appearance of UI elements. JSX gets transformed into regular JavaScript function calls that create React elements. It provides a more declarative and intuitive way to define component hierarchies and interactivity, combining the power of JavaScript with the flexibility of HTML-like syntax."
+    },
+    {
+        "id": 10,
+        "type": "multipleChoice",
+        "question": "What is the purpose of the useContext hook in React?",
+        "options": [
+            "To memoize expensive calculations",
+            "To create reusable custom hooks",
+            "To handle form validation in React applications",
+            "To access the value of a context in functional components"
+        ],
+        "correctAnswer": "To access the value of a context in functional components",
+        "explanation": "The useContext hook in React is used to access the value of a context in functional components. Context provides a way to pass data through the component tree without having to pass props down manually at every level. The useContext hook allows functional components to consume context values directly, making it more convenient to access shared data or state within the component tree. It eliminates the need for prop drilling, where intermediate components pass props without using them directly."
+    },
+    {
+        "id": 11,
+        "type": "multipleChoice",
+        "question": "What is the purpose of the useRef hook in React?",
+        "options": [
+            "To memoize expensive calculations",
+            "To create reusable custom hooks",
+            "To access and manipulate DOM elements",
+            "To handle side effects and perform cleanup in functional components"
+        ],
+        "correctAnswer": "To access and manipulate DOM elements",
+        "explanation": "The useRef hook in React is used to access and manipulate DOM elements. It allows you to create a mutable reference that persists across component renders. With useRef, you can access properties and methods of DOM elements, such as getting their values, adding event listeners, or focusing an input element. Unlike useState, changes to the ref value don't trigger component re-renders, making it useful for scenarios where you need to store and update references to DOM elements without affecting the component's rendering."
+    },
+    {
+        "id": 12,
+        "type": "multipleChoice",
+        "question": "What is the purpose of the useHistory hook in React Router?",
+        "options": [
+            "To memoize expensive calculations",
+            "To create reusable custom hooks",
+            "To handle form validation in React applications",
+            "To manage navigation history and URL in React applications"
+        ],
+        "correctAnswer": "To manage navigation history and URL in React applications",
+        "explanation": "The useHistory hook in React Router is used to manage navigation history and URL in React applications. It provides access to the history object, which allows you to programmatically navigate, push or replace URLs, and go back or forward in the navigation history. By using useHistory, you can leverage React Router's navigation capabilities within functional components. It enables dynamic routing and seamless navigation between different views or pages in a React application."
+    },
+    {
+        "id": 13,
+        "type": "multipleChoice",
+        "question": "What is the purpose of the useReducer hook in React?",
+        "options": [
+            "To memoize expensive calculations",
+            "To create reusable custom hooks",
+            "To handle form validation in React applications",
+            "To manage complex state and state transitions in functional components"
+        ],
+        "correctAnswer": "To manage complex state and state transitions in functional components",
+        "explanation": "The useReducer hook in React is used to manage complex state and state transitions in functional components. It is an alternative to useState and provides a way to handle more advanced state management scenarios. By specifying a reducer function and an initial state, useReducer allows you to define how state changes in response to dispatched actions. It provides a predictable way to update state and manage more complex data structures or state transitions within a component."
+    },
+    {
+        "id": 14,
+        "type": "multipleChoice",
+        "question": "What is the purpose of the useParams hook in React Router?",
+        "options": [
+            "To memoize expensive calculations",
+            "To create reusable custom hooks",
+            "To handle form validation in React applications",
+            "To access URL parameters in React Router"
+        ],
+        "correctAnswer": "To access URL parameters in React Router",
+        "explanation": "The useParams hook in React Router is used to access URL parameters in React Router. It allows you to extract dynamic segments from the current URL and access them as named parameters. For example, if you have a route like '/users/:id', you can use useParams to retrieve the value of 'id' from the URL. This can be useful when building dynamic routes and extracting information from the URL to customize the behavior or content of your components."
+    },
+    {
+
+        "id": 15,
+        "type": "multipleChoice",
+        "question": "What is the purpose of the useLayoutEffect hook in React?",
+        "options": [
+            "To memoize expensive calculations",
+            "To create reusable custom hooks",
+            "To handle form validation in React applications",
+            "To perform synchronous side effects after the DOM has been updated"
+        ],
+        "correctAnswer": "To perform synchronous side effects after the DOM has been updated",
+        "explanation": "The useLayoutEffect hook in React is similar to the useEffect hook, but it fires synchronously after all DOM mutations are applied. It allows you to perform immediate side effects that rely on the updated DOM, such as measuring DOM nodes or updating layout-related properties. This hook is particularly useful for scenarios where you need to synchronously interact with the DOM after a component has rendered or updated. However, it's important to note that excessive use of useLayoutEffect can negatively impact performance, so it should be used sparingly."
+    },
+    {
+        "id": 16,
+        "type": "multipleChoice",
+        "question": "What is the purpose of the withRouter higher-order component in React Router?",
+        "options": [
+            "To memoize expensive calculations",
+            "To create reusable custom hooks",
+            "To handle form validation in React applications",
+            "To provide access to the history object and match params outside of Route components"
+        ],
+        "correctAnswer": "To provide access to the history object and match params outside of Route components",
+        "explanation": "The withRouter higher-order component in React Router is used to provide access to the history object and match params outside of Route components. By wrapping a component with withRouter, you can access the history, location, and match objects, even if the component is not directly rendered by a Route component. This can be useful when you need to access routing-related information and perform navigation actions within a component that is not part of the route hierarchy, such as in nested components or higher-level containers."
+    },
+    {
+        "id": 17,
+        "type": "multipleChoice",
+        "question": "What is the purpose of the react-router-dom package in React?",
+        "options": [
+            "To memoize expensive calculations",
+            "To create reusable custom hooks",
+            "To handle form validation in React applications",
+            "To provide routing capabilities and components for React applications"
+        ],
+        "correctAnswer": "To provide routing capabilities and components for React applications",
+        "explanation": "The react-router-dom package is a popular library in React that provides routing capabilities and components for building single-page applications. It is built on top of React Router and offers a higher-level API for declaratively defining routes, managing navigation, and rendering different components based on the current URL. The package includes components like Route, Link, Switch, and BrowserRouter that enable developers to implement client-side routing and create navigation flows within their React applications."
+    },
 ]
